660e7d3409de411170f56cd9ded0c579f4d0e726
harsha.vardhan <harsha.vardhan@capillarytech.com>
2020-01-03 12:07:00 +0530

    Updating Feedback - Test 2

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,5 @@
 //= require ./app/_lang
 //= require ./app/_search
 //= require ./app/_toc
+//= require ./app/_custom
+
---
+++ source/javascripts/app/_custom.js
@@ -0,0 +1,19 @@
+﻿/* Custom JS */
+
+$(document).ready(function () {
+    
+
+    $('.feedback--submit').click(function () {
+        sendData(sender = $('#feedback-email').val(), feedback = $('#feedback-description').val());
+        $('.feedback--container').fadeOut();
+    });
+
+    $('.fab--button__feedback').click(function () {
+        $('.feedback--container').fadeIn();
+    });
+
+    $('.feedback--modal-close').click(function () {
+        $('.feedback--container').fadeOut();
+    });
+
+});
